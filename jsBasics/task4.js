d01bf593a7d93ca57126aaae53b5ca6b435f624f
Konstantin Sadovets <sadovec.k@gmail.com>
2025-01-17 00:10:52 +0200

    javaScript basic task4

---
+++ jsBasics/task4.js
@@ -0,0 +1,19 @@
+//task 4.1
+let radius = 5;
+
+console.log("Task 4.1 \n\t Area of ​​a circle is " 
+    + (Math.PI * Math.pow(radius, 2)).toFixed(2));
+
+//task 4.2
+let lenght = 7;
+let width = 5;
+
+console.log("Task 4.2 \n\t Area of ​​a rectangle is " 
+    + (lenght * width).toFixed(2));
+
+//task 4.3
+let radiusOfCylinder = 3;
+let height = 4;
+
+console.log("Task 4.3 \n\t Сylinder volume is " 
+    + (Math.PI * Math.pow(radiusOfCylinder, 2) * height).toFixed(2));
