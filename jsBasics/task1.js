5ccd0b6b1fc535aeb4995bf2052024775e3f8890
Konstantin Sadovets <sadovec.k@gmail.com>
2025-01-16 22:51:44 +0200

    javaScript Basic task1

---
+++ jsBasics/task1.js
@@ -0,0 +1,11 @@
+let var_string = "String example";
+let var_number = 5;
+let var_boolean = true;
+let var_undefined = undefined;
+let var_null = null;
+
+console.log(var_string);
+console.log(var_number);
+console.log(var_boolean);
+console.log(var_undefined);
+console.log(var_null);
