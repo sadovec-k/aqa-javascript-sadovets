49bf4f2910719b1e22e139f17cc809ea9beb0cb4
Konstantin Sadovets <sadovec.k@gmail.com>
2025-01-17 00:30:52 +0200

    Fixed task3.js

--- jsBasics/task3.js
+++ jsBasics/task3.js
@@ -1,5 +1,5 @@
 let age = 15;
-let isMajor = age > 18;
+let isMajor = age >= 18;
 
 console.log("Person age is " + age + ",\n" + "isMajor parameter is " + isMajor);
 console.log(`Persone age is ${age}. This person is ${isMajor ? "" : "not"} major.`);
