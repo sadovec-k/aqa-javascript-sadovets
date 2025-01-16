c87318adb1261872f42b7e48cc5cc39731edaf7d
Konstantin Sadovets <sadovec.k@gmail.com>
2025-01-16 23:13:33 +0200

    javaScript basic task2

---
+++ jsBasics/task2.js
@@ -0,0 +1,8 @@
+let firstUser = "Tom";
+let secondUser = "Jerry"
+
+let resultGreetings = "Hello " + firstUser + " and hello " + secondUser + "!";
+console.log(resultGreetings); 
+
+let templateGreetings = `Hello ${firstUser} and hello ${secondUser}!`;
+console.log(templateGreetings);
