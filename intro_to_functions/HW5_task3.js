c6406e01edf72ddde26b9b5fdb4c8bac55b6b3da
Konstantin Sadovets <sadovec.k@gmail.com>
2025-01-24 01:11:08 +0200

    HW5_intro_to_function_updated_task3

--- intro_to_functions/HW5_task3.js
+++ intro_to_functions/HW5_task3.js
@@ -9,15 +9,3 @@ function checkIsGoodsAvailable(available, ordered){
 console.log(checkIsGoodsAvailable(10, 0));
 console.log(checkIsGoodsAvailable(10, 15));
 console.log(checkIsGoodsAvailable(10, 7));
-
-
-function reverseString(str) {
-    if (str == "") return "";
-    let result = "";
-    for (let i = str.length - 1; i >= 0; i --){
-        result = result + str[i];
-    }
-    return result;
-}
-
-console.log("Result " + reverseString("!"));
