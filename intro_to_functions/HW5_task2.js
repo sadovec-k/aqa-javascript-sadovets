0d0f91aa51b18f501b1ced3a0450bf8538e5f8b0
Konstantin Sadovets <sadovec.k@gmail.com>
2025-01-24 00:23:47 +0200

    HW5_intro_to_function_task2

---
+++ intro_to_functions/HW5_task2.js
@@ -0,0 +1,6 @@
+function isAdult(age) {
+    return age >= 18;
+}
+
+console.log(`This person is${isAdult(15) ? "" : " not"} adult`);
+console.log(`This person is${isAdult(25) ? "" : " not"} adult`);
