05e8793aadb151afe50262d4e89c7fd3fe8c65ee
Konstantin Sadovets <sadovec.k@gmail.com>
2025-01-25 20:46:43 +0200

    HW6_advanced_function_usage_task1

---
+++ advanced_function_usage/HW6_task1.js
@@ -0,0 +1,15 @@
+function handleEven(num){
+    console.log(`Number ${num} is even`);
+}
+
+function handleOdd(num){
+    console.log(`Number ${num} is odd`);
+}
+
+function handleNum(num, printEven, printOdd) {
+    return num % 2 == 0 
+        ? printEven(num)
+        : printOdd(num);
+}
+
+handleNum(7, handleEven, handleOdd);
