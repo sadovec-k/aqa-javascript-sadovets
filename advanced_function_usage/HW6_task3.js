4c6e62fe1c827ed8de224722788ec7bb5a701b69
Konstantin Sadovets <sadovec.k@gmail.com>
2025-01-25 23:26:14 +0200

    HW6_advanced_function_usage_task3

---
+++ advanced_function_usage/HW6_task3.js
@@ -0,0 +1,23 @@
+function divide(numerator, denominator) {
+    if (!Number.isFinite(numerator) || !Number.isFinite(denominator)) { 
+        throw new Error("Input isn't a number"); 
+    }
+
+    if (denominator == 0) throw new Error("denominator is 0");
+
+    return numerator / denominator;
+}
+
+function callDivide (numerator, denominator) {
+    try{
+        console.log(divide(numerator, denominator));
+    } catch (e) {
+        console.log(e);
+    } finally {
+        console.log("Work is over");
+    }
+}
+
+callDivide(21, 3);
+callDivide(15, 0);
+callDivide("ew", 3);
