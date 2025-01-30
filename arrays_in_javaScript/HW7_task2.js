a571bccbc195e00238e5e2ed10ac023e939aca53
sadovec-k <sadovec.k@gmail.com>
2025-01-31 00:18:32 +0200

    Update arrays_in_javaScript/HW7_task2.js
    Fix
    
    Co-authored-by: Kwazimodd <55921352+Kwazimodd@users.noreply.github.com>

--- arrays_in_javaScript/HW7_task2.js
+++ arrays_in_javaScript/HW7_task2.js
@@ -1,4 +1,4 @@
 const numbers = [9, 3, 7, 4, 0, 11];
 
-const newNumbers = numbers.map((number) => number * numbers.indexOf(number))
+const newNumbers = numbers.map((number, index) => number * index)
 console.log("New array " + newNumbers);
