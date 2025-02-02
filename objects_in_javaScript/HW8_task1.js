e7b67ce2622824dfeecfe284ae6091bf5aa6069d
Konstantin Sadovets <sadovec.k@gmail.com>
2025-02-02 15:42:11 +0200

    HW8 objects in javaScript task1

---
+++ objects_in_javaScript/HW8_task1.js
@@ -0,0 +1,8 @@
+const student = {};
+
+student.firstName = "John";
+student.lastName = "Wotson";
+student.age = "20";
+student.courses = ["History", "Medicine", "Mathematics"];
+
+console.log(student);
