0f5a079b4cd6c107c5014d7dfc1e920b141e8560
Konstantin Sadovets <sadovec.k@gmail.com>
2025-02-16 23:58:26 +0200

    HW11_updated code style

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ import pluginJs from "@eslint/js";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {rules:{"semi": "error", 
-    "no-console": "warn", 
+    "no-console": "off", 
     "no-unused-vars": "off" }, languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
 ];
