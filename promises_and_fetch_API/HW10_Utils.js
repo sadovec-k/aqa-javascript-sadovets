5e3af64c7e89da90b161c725d98e9ca8bf68b492
sadovec-k <sadovec.k@gmail.com>
2025-02-16 18:41:24 +0200

    Update promises_and_fetch_API/HW10_Utils.js
    Co-authored-by: Kwazimodd <55921352+Kwazimodd@users.noreply.github.com>

--- promises_and_fetch_API/HW10_Utils.js
+++ promises_and_fetch_API/HW10_Utils.js
@@ -14,6 +14,6 @@ export default class Utils {
     static async getUrl(url) {
         const response = await fetch(url);
         const data = await response.json();
-        return console.log(data); 
+        return data; 
     }
 }
